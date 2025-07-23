6df8ec57887bce5c5dd56ef0504aa5e1b1be797f
GabrielGJU7 <gjacoboulloa@gmail.com>
2025-07-22 21:17:55 -0600

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielgju/godesdecero
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gabrielgju/godesdecero/variables"
+
+func main() {
+	variables.MostrarEnteros()
+}
